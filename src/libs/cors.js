94f961481d3ccd57b87e752e5c109d040dfab10f
NOX-0202 <fernandinho.nco@gmail.com>
2022-05-13 09:21:59 -0300

    resolving cors

--- src/libs/cors.js
+++ src/libs/cors.js
@@ -1,6 +1,6 @@
 import NextCors from "nextjs-cors";
 
-export default async (req, res) => {
+export default async function cors(req, res) => {
     await NextCors(req, res, {
         // Options
         methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
