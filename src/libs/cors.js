7d6c2ff23280e41778e04910b73e495dc8fc0711
NOX-0202 <fernandinho.nco@gmail.com>
2022-06-01 19:17:31 -0300

    vps test

--- src/libs/cors.js
+++ src/libs/cors.js
@@ -1,6 +1,7 @@
 import NextCors from "nextjs-cors";
 
 export default async function cors(req, res) {
+    console.log('in-CORS')
     await NextCors(req, res, {
         // Options
         methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
